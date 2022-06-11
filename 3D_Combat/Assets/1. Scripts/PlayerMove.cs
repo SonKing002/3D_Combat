9435988eb597555cd3ebcedcb5241e21ed357ca1
jaeHyuek <gur00229@gmail.com>
2022-06-11 13:55:53 +0900

    6/10

---
+++ 3D_Combat/Assets/1. Scripts/PlayerMove.cs
@@ -0,0 +1,33 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Main;
+
+namespace Main
+{
+    public class PlayerMove : MonoBehaviour
+    {
+        //플래이어 움직임
+
+        float h, v, moveSpeed, limitSpeed, jumpPower, limitJump;
+        bool isGround;
+        Rigidbody rig;
+
+        void Start()
+        {
+
+        }
+
+        void MoveCtrl()
+        { 
+            
+        }
+
+        void Update()
+        {
+
+        }
+    }
+
+}
+
