94085eae330fd9c934e658b7221e5af8fcd859b8
jaeHyuek <gur00229@gmail.com>
2022-07-24 14:00:08 +0900

    0724

--- 3D_Combat/Assets/1. Scripts/PlayerMove.cs
+++ 3D_Combat/Assets/1. Scripts/PlayerMove.cs
@@ -9,32 +9,43 @@ namespace Main
     {
         //플래이어 움직임
 
-        float h, v, moveSpeed, limitSpeed, jumpPower, limitJump;
+        public float h, z, moveSpeed, limitSpeed, jumpPower, limitJump;
         Vector3 dir;
         bool isGround;
         CharacterController cc;
 
         void Start()
         {
+            //할당 초기화
             cc = GetComponent<CharacterController>();
+            dir = new Vector3(0,0,0);
         }
 
         void MoveCtrl()
         {
             h = Input.GetAxis("Horizontal");
-            v = Input.GetAxis("Vertical");
-            dir = new Vector3(h, 0, v);
+            z = Input.GetAxis("Vertical");
 
+            //위 아래 셋팅
+            dir = new Vector3(h, 0, z);
+
+            //대각선 속도 일정
             dir.Normalize();
 
+            //로컬 좌표계를 월드 좌표계로 변환
             dir = transform.TransformDirection(dir);
-            
+
+            cc.Move(dir);
 
         }
 
+        private void FixedUpdate()
+        {
+            MoveCtrl();
+        }
         void Update()
         {
-
+            
         }
     }
 
